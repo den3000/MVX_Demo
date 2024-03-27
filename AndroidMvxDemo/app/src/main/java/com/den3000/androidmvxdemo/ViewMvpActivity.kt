829deabf118a8f7a182a342ac675542f33c607c9
Denis Suprun <suprundenis@gmail.com>
2024-03-27 22:53:34 +0300

    + Initial ViewMvpActivity setup

--- AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/ViewMvpActivity.kt
+++ AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/ViewMvpActivity.kt
@@ -2,10 +2,48 @@ package com.den3000.androidmvxdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.den3000.androidmvxdemo.databinding.ActivityViewBinding
 
-class ViewMvpActivity : AppCompatActivity() {
+class ViewMvpActivity : AppCompatActivity(), View.OnClickListener, TextWatcher {
+
+    private lateinit var binding: ActivityViewBinding
+
+    private var adapter: ItemsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_mvp)
+        binding = ActivityViewBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvTitle.text = "View MVP"
+        binding.btClearSearch.setOnClickListener(this)
+        binding.etSearchString.addTextChangedListener(this)
+
+        adapter = ItemsAdapter(emptyList())
+        binding.rvItems.layoutManager = LinearLayoutManager(this)
+        binding.rvItems.adapter = adapter
     }
+
+    //region TextWatcher
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+    override fun onTextChanged(cs: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        //
+    }
+
+    override fun afterTextChanged(p0: Editable?) { }
+    //endregion
+
+    //region View.OnClickListener
+    override fun onClick(view: View?) {
+        when(view) {
+            binding.btClearSearch -> { }
+        }
+    }
+    //endregion
 }
