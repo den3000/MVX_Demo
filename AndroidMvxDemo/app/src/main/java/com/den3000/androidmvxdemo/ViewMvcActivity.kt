ed4391a79984ac8416024e311f54ab1a2c5e70d2
Denis Suprun <suprundenis@gmail.com>
2024-03-27 18:24:15 +0300

    + Adding some basic UI

--- AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/ViewMvcActivity.kt
+++ AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/ViewMvcActivity.kt
@@ -1,11 +1,19 @@
 package com.den3000.androidmvxdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.den3000.androidmvxdemo.databinding.ActivityViewMvcBinding
 
 class ViewMvcActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityViewMvcBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_mvc)
+        binding = ActivityViewMvcBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvTitle.text = "View MVC"
     }
 }
