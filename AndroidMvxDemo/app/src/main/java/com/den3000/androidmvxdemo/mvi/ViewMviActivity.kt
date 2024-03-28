fef02a8db6615f8885ea43c8b98581a0f3173260
Denis Suprun <suprundenis@gmail.com>
2024-03-28 16:49:47 +0300

    + ViewMviActivity basic implementation

--- AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/mvi/ViewMviActivity.kt
+++ AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/mvi/ViewMviActivity.kt
@@ -1,12 +1,55 @@
 package com.den3000.androidmvxdemo.mvi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.den3000.androidmvxdemo.R
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.den3000.androidmvxdemo.databinding.ActivityViewBinding
+import com.den3000.androidmvxdemo.shared.ItemsAdapter
 
-class ViewMviActivity : AppCompatActivity() {
+class ViewMviActivity : AppCompatActivity(),
+    View.OnClickListener,
+    TextWatcher
+{
+
+    private lateinit var binding: ActivityViewBinding
+
+    private var adapter: ItemsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_mvi)
+        binding = ActivityViewBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvTitle.text = "View MVI"
+        binding.btClearSearch.setOnClickListener(this)
+        binding.etSearchString.addTextChangedListener(this)
+
+        adapter = ItemsAdapter(emptyList())
+        binding.rvItems.layoutManager = LinearLayoutManager(this)
+        binding.rvItems.adapter = adapter
     }
+
+    //region TextWatcher
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+    override fun onTextChanged(cs: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//        viewModel.onSearchTextChanged(cs)
+    }
+
+    override fun afterTextChanged(p0: Editable?) { }
+    //endregion
+
+    //region View.OnClickListener
+    override fun onClick(view: View?) {
+        when(view) {
+            binding.btClearSearch -> {
+//                viewModel.onClearPressed()
+            }
+        }
+    }
+    //endregion
 }
