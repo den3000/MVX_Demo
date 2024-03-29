da192bc67a4c2b5a7f09da01519a2bb3cfdae3e6
Denis Suprun <suprundenis@gmail.com>
2024-03-29 18:02:45 +0300

    + Integrating ItemsIntenter into ComposeMviActivity

--- AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/mvi/ComposeMviActivity.kt
+++ AndroidMvxDemo/app/src/main/java/com/den3000/androidmvxdemo/mvi/ComposeMviActivity.kt
@@ -3,44 +3,46 @@ package com.den3000.androidmvxdemo.mvi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.den3000.androidmvxdemo.shared.ComposeView
 import com.den3000.androidmvxdemo.ui.theme.AndroidMvxDemoTheme
 
 class ComposeMviActivity : ComponentActivity() {
+
+    private val intenter: ItemsIntenter by viewModels { ItemsIntenter.Factory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val viewState = intenter.viewState.observeAsState(initial = ItemsIntenter.ViewState(
+                isShowProgress = false,
+                isShowResults = true,
+                searchText = "",
+                dataset = emptyList()
+            ))
+
             AndroidMvxDemoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Compose MVI")
+                    ComposeView(
+                        title = "Compose MVVM",
+                        isShowProgress = viewState.value.isShowProgress,
+                        isShowResults = viewState.value.isShowResults,
+                        text = viewState.value.searchText,
+                        onTextChanged = { intenter.obtain(ItemsIntenter.ViewEvent.TextChanged(it)) },
+                        onClear = { intenter.obtain(ItemsIntenter.ViewEvent.ClearText) },
+                        dataset = viewState.value.dataset
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    AndroidMvxDemoTheme {
-        Greeting2("Compose MVI")
     }
 }
